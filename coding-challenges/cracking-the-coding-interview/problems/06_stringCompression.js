a617acbb7fa5100dc62e103e7003d84739cebc38
Nicolas-Melluso <nicolas.e.melluso@gmail.com>
2024-08-21 18:06:10 -0300

    resolve using a counter and comparison ternary

--- coding-challenges/cracking-the-coding-interview/problems/06_stringCompression.js
+++ coding-challenges/cracking-the-coding-interview/problems/06_stringCompression.js
@@ -1,24 +1,24 @@
+"use strict";
 // 6. *String Compression*:
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Implement a method to perform basic string compression using the counts of repeated characters.
 // For example, the string aabcccccaaa would become a2blc5a3,
 // If the "compressed" string would not become smaller than the original string,
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
-
-export default function stringCompression (str: string) : string {
-    let newStr = [];
-    let counter = 1;
-
-    for (let i = 0; i < str.length; i++) {
+function stringCompression(str) {
+    var newStr = [];
+    var counter = 1;
+    for (var i = 0; i < str.length; i++) {
         if (str[i] === str[i + 1]) {
             counter++;
-        } else {
+        }
+        else {
             newStr.push(str[i] + counter);
             counter = 1;
         }
     }
-
-    let strFormatted = newStr.join('');
+    var strFormatted = newStr.join('');
     return strFormatted.length < str.length ? strFormatted : str;
 }
+exports.default = stringCompression;
