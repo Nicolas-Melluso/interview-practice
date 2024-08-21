07f32989f5dfb8c34286e1f95d1f4cbf6a719114
Nicolas-Melluso <nicolas.e.melluso@gmail.com>
2024-08-20 22:33:08 -0300

    try to resolve with replaceAll but resolve with split and join

--- coding-challenges/cracking-the-coding-interview/problems/03_urlify.js
+++ coding-challenges/cracking-the-coding-interview/problems/03_urlify.js
@@ -1,9 +1,10 @@
+"use strict";
 // 3.  URLify:
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Write a method to replace all spaces in a string with '%20'.
 // You may assume that the string has sufficient space at the end to hold the additional characters,
 // and that you are given the "true" length of the string.
-
-export default function URLify (s1 : string): string {
+function URLify(s1) {
     return s1.split(' ').join('%20');
 }
+exports.default = URLify;
