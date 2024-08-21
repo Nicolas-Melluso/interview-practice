d987d6a04e0f7ba683b05b2641a3aaf96f67e8fa
Nicolas-Melluso <nicolas.e.melluso@gmail.com>
2024-08-20 23:16:13 -0300

    SC-04_resolve using odd operation and hash record

--- coding-challenges/cracking-the-coding-interview/problems/04_palindromePermutation.js
+++ coding-challenges/cracking-the-coding-interview/problems/04_palindromePermutation.js
@@ -1,5 +1,6 @@
+"use strict";
 // 4. *Palindrome Permutation*: 
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Given a string, write a function to check if it is a permutation of a palindrome.
 // A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.
 // The palindrome does not need to be limited to just dictionary words.
@@ -8,33 +9,26 @@
 // Input: Tact Coa
 // Output True (permutations: "taco cat", "atco cta", etc.)
 // ```
-
-export default function palindromePermutation (str: string): boolean {
+function palindromePermutation(str) {
     str = str.replace(/ /g, '').toLowerCase();
-    
-
     if (str.length === 1) {
         return true;
     }
-
-    const counts = new Map<string, number>();
-
-    for (let i = 0; i < str.length; i++) {
-        const strAux = str[i];
+    var counts = new Map();
+    for (var i = 0; i < str.length; i++) {
+        var strAux = str[i];
         counts.set(strAux, (counts.get(strAux) || 0) + 1);
     }
-
-    const keys = Array.from(counts.keys());
-
-    let oddCount = 0;
-
-    for (let i = 0; i < keys.length; i++) {
-        const count = counts.get(keys[i]) || 0;
+    var keys = Array.from(counts.keys());
+    var oddCount = 0;
+    for (var i = 0; i < keys.length; i++) {
+        var count = counts.get(keys[i]) || 0;
         if (count % 2 !== 0) {
             oddCount++;
         }
-        if (oddCount > 1) return false
+        if (oddCount > 1)
+            return false;
     }
-
     return true;
 }
+exports.default = palindromePermutation;
