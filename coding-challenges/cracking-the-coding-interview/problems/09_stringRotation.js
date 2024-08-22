5816ca956fd6c8855994638cd530fe248e6be7c4
Nicolas-Melluso <nicolas.e.melluso@gmail.com>
2024-08-22 10:56:15 -0300

    a propperty of strings is that every rotation in s2 is included in s1+s1

--- coding-challenges/cracking-the-coding-interview/problems/09_stringRotation.js
+++ coding-challenges/cracking-the-coding-interview/problems/09_stringRotation.js
@@ -1,19 +1,17 @@
+"use strict";
 // 9. *String Rotation*;
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Assume you have a method isSubstring which checks if one word is a substring of another.
 // Given two strings, si and s2, write code to check if s2 is a rotation of 1i using only one call to isSubstring
 // [e.g., "waterbottle" is a rotation oP'erbottlewat")
-
-function isSubstring(s1: string, s2: string): boolean {
+function isSubstring(s1, s2) {
     return s1.includes(s2);
 }
-
-export default function stringRotation(s1: string, s2: string): boolean {
+function stringRotation(s1, s2) {
     if (s1.length !== s2.length) {
         return false;
     }
-
-    const concatenated = s1 + s1;
-
+    var concatenated = s1 + s1;
     return isSubstring(concatenated, s2);
 }
+exports.default = stringRotation;
